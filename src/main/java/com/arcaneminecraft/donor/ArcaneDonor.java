b935953997adef3b006cf9fd11f5cdc1a8855b45
Simon Chuu <simonorj@outlook.com>
2018-07-03 20:21:29 -0400

    Update API + build.gradle: Include all libs as Libraries

--- src/main/java/com/arcaneminecraft/donor/ArcaneDonor.java
+++ src/main/java/com/arcaneminecraft/donor/ArcaneDonor.java
@@ -1,6 +1,6 @@
 package com.arcaneminecraft.donor;
 
-import com.arcaneminecraft.api.ColorPalette;
+import com.arcaneminecraft.api.ArcaneColor;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -39,58 +39,63 @@ public class ArcaneDonor extends JavaPlugin {
         if (cmd.getName().equalsIgnoreCase("donor")) {
 
             sender.sendMessage("Donor commands: /bbycake, /dclem, /monreflexion, /sharpshootingace, /simonorj, /ytorgonak");
+            // TODO: Get list of all donors from LuckPerms
+
 
             return true;
         }
         return false;
     }
 
+
+    // TODO: Move this into ArcaneBungee
     public final class DonorEvents implements Listener {
+        private final String[] DONOR = {
+                "You're pretty awesome. Seriously.",
+                "You're pretty awesome. Seriously. 100% awesome.",
+                "By donating, you've helped make Arcane possible. " + ChatColor.BOLD + "Thanks!",
+                "Welcome back to Arcane.",
+                "You're way cooler than everybody else.",
+                "Thank you for your support!",
+                "You should make use of your powerful /slap command.",
+                "Thank you!",
+                "Thank you for supporting Arcane!",
+                "Don't forget, you have access to /slap. Use it wisely.",
+                "You can hide yourself from the Dynmap via /dynmap hide.",
+                "Did you know there's a donor only section on the forums?",
+                "Welcome back to Arcane Survival!",
+                "We love you.",
+                "We love you. A lot.",
+                "Tip: To reappear from the Dynmap if you've hidden yourself, type /dynmap show.",
+                "Thank you.",
+                "If you'd like to hide yourself from our Dynmap, type /dynmap hide.",
+                "Did we tell you you're awesome? You really are.",
+                //"You're pretty awesome. Not as awesome as Agentred100 is, though.", // <.<
+                //"You're pretty awesome. Almost as awesome as _NickV, keep it up.",  // >.>
+                "Thank you. You're awesome.",
+                "Welcome back to Arcane Survival.",
+                "You're a pretty cool person.",
+                "What cool stuff can we give to our donors? Let us know on the forums.",
+                "We appreciate your support.",
+                "If you're looking for some building ideas, you can type /dclem.",
+                "We appreciate your support.",
+        };
+
+        // To be moved to new class along with cleanup updates
+        private String getRandomDonorMessage() {
+            Random rand = new Random();
+            return DONOR[rand.nextInt(DONOR.length)];
+        }
+
         @EventHandler(priority = EventPriority.NORMAL)
         public void donorJoin(PlayerJoinEvent e) {
             if (e.getPlayer().hasPermission("arcane.donor")) {
                 Player p = e.getPlayer();
-                p.sendMessage(ColorPalette.DONOR + " You are a donor. " + ColorPalette.CONTENT + getRandomDonorMessage());
+                p.sendMessage(ArcaneColor.DONOR + " You are a donor. " + ArcaneColor.CONTENT + getRandomDonorMessage());
                 p.sendMessage("");
             }
         }
     }
 
 
-    // To be moved to new class along with cleanup updates
-    private static String getRandomDonorMessage() {
-        Random rand = new Random();
-        return DONOR[rand.nextInt(DONOR.length)];
-    }
-
-    private static final String[] DONOR = {
-            "You're pretty awesome. Seriously.",
-            "You're pretty awesome. Seriously. 100% awesome.",
-            "By donating, you've helped make Arcane possible. " + ChatColor.BOLD + "Thanks!",
-            "Welcome back to Arcane.",
-            "You're way cooler than everybody else.",
-            "Thank you for your support!",
-            "You should make use of your powerful /slap command.",
-            "Thank you!",
-            "Thank you for supporting Arcane!",
-            "Don't forget, you have access to /slap. Use it wisely.",
-            "You can hide yourself from the Dynmap via /dynmap hide.",
-            "Did you know there's a donor only section on the forums?",
-            "Welcome back to Arcane Survival!",
-            "We love you.",
-            "We love you. A lot.",
-            "Tip: To reappear from the Dynmap if you've hidden yourself, type /dynmap show.",
-            "Thank you.",
-            "If you'd like to hide yourself from our Dynmap, type /dynmap hide.",
-            "Did we tell you you're awesome? You really are.",
-            //"You're pretty awesome. Not as awesome as Agentred100 is, though.", // <.<
-            //"You're pretty awesome. Almost as awesome as _NickV, keep it up.",  // >.>
-            "Thank you. You're awesome.",
-            "Welcome back to Arcane Survival.",
-            "You're a pretty cool person.",
-            "What cool stuff can we give to our donors? Let us know on the forums.",
-            "We appreciate your support.",
-            "If you're looking for some building ideas, you can type /dclem.",
-            "We appreciate your support.",
-    };
 }
