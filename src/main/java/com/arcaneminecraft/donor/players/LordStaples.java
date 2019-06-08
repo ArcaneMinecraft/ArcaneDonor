a32498795063285b11a50ac3a4f473c9a36b9814
Simon Chuu <simonorj@outlook.com>
2019-06-08 00:01:51 -0400

    LordStaples: Make Accent Yellow

--- src/main/java/com/arcaneminecraft/donor/players/LordStaples.java
+++ src/main/java/com/arcaneminecraft/donor/players/LordStaples.java
@@ -13,11 +13,9 @@ import java.util.List;
 public class LordStaples implements TabExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        sender.sendMessage(ChatColor.YELLOW.toString() + ChatColor.BOLD + '>'
-                + ChatColor.DARK_GRAY + ChatColor.BOLD + '>'
+        sender.sendMessage(ChatColor.YELLOW.toString() + ChatColor.BOLD + ">>"
                 + ChatColor.RED + ChatColor.BOLD  + " Glory to the Auric Empire! "
-                + ChatColor.DARK_GRAY + ChatColor.BOLD + '<'
-                + ChatColor.YELLOW + ChatColor.BOLD + '<');
+                + ChatColor.YELLOW + ChatColor.BOLD + "<<");
         if (sender instanceof Player) {
             Player p = (Player) sender;
             p.playSound(p.getLocation(), Sound.ENTITY_LIGHTNING_BOLT_THUNDER, 1.0f, 1.0f);
