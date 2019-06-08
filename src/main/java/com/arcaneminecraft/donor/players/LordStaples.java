01b0586bb4c33b50af41ae054cf1d9212f04d80a
Simon Chuu <simonorj@outlook.com>
2019-06-07 23:36:20 -0400

    LordStaples: Add Accent

--- src/main/java/com/arcaneminecraft/donor/players/LordStaples.java
+++ src/main/java/com/arcaneminecraft/donor/players/LordStaples.java
@@ -3,7 +3,6 @@ package com.arcaneminecraft.donor.players;
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
@@ -14,7 +13,7 @@ import java.util.List;
 public class LordStaples implements TabExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        sender.sendMessage(ChatColor.RED + "Glory to the Auric Empire!");
+        sender.sendMessage(ChatColor.RED + ">> Glory to the Auric Empire! <<");
         if (sender instanceof Player) {
             Player p = (Player) sender;
             p.stopSound(Sound.ENTITY_LIGHTNING_BOLT_THUNDER); // TODO: Test if this works
