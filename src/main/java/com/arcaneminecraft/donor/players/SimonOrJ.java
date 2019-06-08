97263b7b21193ad1d22bb5a6306607b3cdd9e24b
Simon Chuu <simonorj@outlook.com>
2019-06-07 22:39:43 -0400

    SimonOrJ: Modify command output

--- src/main/java/com/arcaneminecraft/donor/players/SimonOrJ.java
+++ src/main/java/com/arcaneminecraft/donor/players/SimonOrJ.java
@@ -3,22 +3,26 @@ package com.arcaneminecraft.donor.players;
 import com.arcaneminecraft.donor.ArcaneDonor;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 
+import java.util.Collections;
 import java.util.Formatter;
+import java.util.List;
 import java.util.Random;
 
-public class SimonOrJ implements CommandExecutor {
+public class SimonOrJ implements TabExecutor {
     private final ArcaneDonor plugin = ArcaneDonor.getInstance();
     private static final String[] FORMAT = {
-            "You roar at Simon after %s",
-            "Meow meow %s meow!",
-            "I ain't lying when I said %s said it first!",
-            "%s was here",
-            "%s already ran this command!",
-            "Too late! %s claimed this before you!"
+            "%s says roar at Simon!",
+            "%s says meow meow meow...",
+            "Hunt down %s!",
+            "Message %s and tell him to run this command!",
+            "%s says poke (or punch?) Simon!",
+            "%s says jump 2 times!",
+            "%s says touch the sky!",
+            "Look at %s"
     };
     /**
      * Player who ran this command last
@@ -45,9 +49,14 @@ public class SimonOrJ implements CommandExecutor {
             plugin.getConfig().set("simonorj.last", currentPlayer);
         }
 
-        sender.sendMessage(ChatColor.DARK_GREEN + "[SimonOrJ] " + ChatColor.AQUA
+        sender.sendMessage(ChatColor.DARK_GREEN + "[SimonSays] " + ChatColor.AQUA
                 + f.format(FORMAT[rand.nextInt(FORMAT.length)], lastPlayer));
 
         return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        return Collections.emptyList();
     }
 }
