015ba958fa0a5b66a85b47bd4cafbcca8e854bf9
Simon Chuu <simonorj@outlook.com>
2019-06-07 22:51:51 -0400

    Bbycake: Add tabcomplete

--- src/main/java/com/arcaneminecraft/donor/players/Bbycake.java
+++ src/main/java/com/arcaneminecraft/donor/players/Bbycake.java
@@ -1,10 +1,13 @@
 package com.arcaneminecraft.donor.players;
 
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 
-public class Bbycake implements CommandExecutor {
+import java.util.Collections;
+import java.util.List;
+
+public class Bbycake implements TabExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (sender.hasPermission("arcane.bbycake")) {
@@ -13,5 +16,10 @@ public class Bbycake implements CommandExecutor {
             sender.sendMessage("You are not bbycake.");
         }
         return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args) {
+        return Collections.emptyList();
     }
 }
